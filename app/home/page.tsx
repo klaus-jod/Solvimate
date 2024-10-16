313458f847ec40d04088cbf6a156b6ed0b1eebcf
nikhil-sen18 <sennikhil221@gmail.com>
2024-10-16 22:47:08 +0530

    MetaData fix

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 const HomePage = () => {
     return (
-        <main className="flex  w-full justify-around items-center  h-[100vh]  bg-slate-300">
+        <main className="flex  justify-center lg:gap-5 items-center h-[100vh] bg-slate-300  ">
             <div className="">
                 <Image
                     src="/service-company.jpg"
@@ -11,7 +11,7 @@ const HomePage = () => {
                     width={500}
                 />
             </div>
-            <div className="flex flex-col space-y-7">
+            <div className="flex flex-col space-y-7  lg:w-[800px] ">
                 <h1 className="text-6xl font-extrabold text-gray-600">Welcome to <span className="text-lime-600">Solvimate</span> <br />Your trusted partner in solutions</h1>
                 <p className="text-lg text-gray-500">At solvimate, we specialized in delivering tailored service disigned to meet your unique needs.</p>
             </div>
